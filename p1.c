67466907ae3667bca9615502e6127740f307b17e
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2024-02-17 09:20:01 +0530

    17/2/24

--- p1.c
+++ p1.c
@@ -1,28 +1,26 @@
-// sum positions
-#include<stdio.h>
-int main()
-{
-    int a[] = {4 , 5 , 2 , 1};
-    int size = sizeof(a) / sizeof(a[0]);
-    int target = 3;
-    int i , j;
-    for(i = 0 ; i < size ; i++)
-    {
-        for(j = i + 1 ; j < size ; j++)
-        {
-            if(a[i] + a[j] == target)
-            {
-                printf("[%d , %d]",i,j);
-                break;
-            }
-            else{
-                printf("position can't found");
-                break;
-            }
-        }
-    }
-    return 0;
-}
+// // sum positions
+// #include<stdio.h>
+// int main()
+// {
+//     int a[] = {4 , 5 , 2 , 1};
+//     int size = sizeof(a) / sizeof(a[0]);
+//     int target = 7;
+//     int f = 1;
+//     int i , j;
+//     for(i = 0 ; i < size ; i++)
+//     {
+//         for(j = i + 1 ; j < size ; j++)
+//         {
+//             if(a[i] + a[j] == target)
+//             {
+//                 printf("[%d , %d]",i,j);
+//                 break;
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
 
 // revese number
 
